c918089872549d82c98844f2a4620e8dcb4ea63f
F0xedb <tommeyers.tm127@gmail.com>
2019-05-22 13:57:31 +0200

    Abstraction of api data

--- frontend/src/app/shared/search.service.ts
+++ frontend/src/app/shared/search.service.ts
@@ -2,32 +2,33 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { data } from "./mocksearch";
+import { environment } from "./../../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class SearchService {
   private BASE_URL = " https://www.googleapis.com/";
-  private API_KEY = "AIzaSyAXFGAj_awXKkT24QkI2zjrULlg0N_exAE";
-  private CX = "017576662512468239146:omuauf_lfve";
+  private API_KEY = environment.API_KEY;
+  private CX = environment.CX;
 
   constructor(private http: HttpClient) {}
 
   getSearchResultList(query, size = 8, page = 0): Observable<any> {
-    return this.http.get(
+    const url =
       this.BASE_URL +
-        "customsearch/v1?key=" +
-        this.API_KEY +
-        "&cx=" +
-        this.CX +
-        "&q=" +
-        query +
-        "&num=" +
-        size +
-        "&start=" +
-        page * size +
-        1
-    );
+      "customsearch/v1?key=" +
+      this.API_KEY +
+      "&cx=" +
+      this.CX +
+      "&q=" +
+      query +
+      "&num=" +
+      size +
+      "&start=" +
+      (page * size + 1);
+    console.log("Requesting url:" + url);
+    return this.http.get(url);
   }
   getMock(query: string, size = 8, page = 0): any {
     return data;
