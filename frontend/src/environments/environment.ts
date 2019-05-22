118605d09927c827d3f2213eb2cad0f6ec627960
F0xedb <tommeyers.tm127@gmail.com>
2019-05-22 13:56:30 +0200

    Put data file into the environment file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: "AIzaSyAXFGAj_awXKkT24QkI2zjrULlg0N_exAE",
+  CX: "017576662512468239146:omuauf_lfve"
 };
 
 /*
